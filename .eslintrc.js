fd15d14e3362a3d0dad14106d76da391e2911b41
andycarrell <carrellandrew@gmail.com>
2022-12-03 13:49:48 +1300

    adds in lint checks too, and CI lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  ignorePatterns: [
+    "/netlify/functions/server",
+    "/node_modules",
+    "/public/build",
+    "/server/build",
+  ],
 };
